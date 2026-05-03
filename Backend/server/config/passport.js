fee96ed1873580b70f17d19cd45e6e4ed3a60686
Vincent Batallones <202310567@gordoncollege.edu.ph>
2026-05-03 22:54:37 +0800

    Fixed path

---
+++ Backend/server/config/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const UsersModel = require('../server/models/users');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+    },
+    async(accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+
+    try {    
+        let user = await UsersModel.findOneAndUpdate({ googleId: profile.id }, {isLoggedIn:true});
+
+        if (!user) {
+            user = await UsersModel.create({
+                googleId: profile.id,
+                username: profile.displayName,
+                email: profile.emails[0].value,
+                avatar: profile.photos[0].value,
+                isVerified: true,
+            })
+        }
+
+        return cb(null, user);
+        
+    } catch (err) {
+        return cb(err, null);
+    }
+
+    }
+));
